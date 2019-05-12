a333028be3c2a4b41619da4141d1c7d4f0cdd154
Brandon Hanlon <give101k@gmail.com>
2019-05-12 16:25:10 -0400

    removed error

--- model/database.php
+++ model/database.php
@@ -6,7 +6,6 @@ $password = 'vAr7mry$$404AvU';
 try {
   $db = new PDO($dsn, $username, $password);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
   $error_message = $e->getMessage();
   include 'database_error.php';
